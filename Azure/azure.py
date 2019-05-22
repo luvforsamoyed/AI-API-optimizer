56477ad5045476137e1e66c70a2fe8731e63a9aa
Doyeong, Kim <46958918+luvforsamoyed@users.noreply.github.com>
2019-05-23 00:19:59 +0900

    Add files via upload

--- Azure/azure.py
+++ Azure/azure.py
@@ -17,20 +17,16 @@ def send_request(endpoint, url, subscription_key, request_data):
     return json.loads(response.content.decode("utf-8"))
 
 def detect_batch(request_data):
-    print("Detecting anomalies as a batch")
     result = send_request(endpoint, batch_detection_url, subscription_key, request_data)
-#     print(json.dumps(result, indent=4))
 
     if result.get('code') != None:
         print("Detection failed. ErrorCode:{}, ErrorMessage:{}".format(result['code'], result['message']))
     else:
         # Find and display the positions of anomalies in the data set
         anomalies = result["isAnomaly"]
-        print("Anomalies detected in the following data positions:")
         true = []
         for x in range(len(anomalies)):
             if anomalies[x] == True:
-                print (x)
                 true.append(x)
                 
     return true
@@ -59,4 +55,28 @@ def f1_metrics(abn_pt, y, dt):
         
     f1 = f1_score(y_true, y_score)
     
-    return f1
+    return f1, y_true, y_score
+
+def vis(y, ts, y_true, y_score):
+
+    tp = []
+    fp = []
+    fn = []
+
+    for i in range(y_true.size):
+        if (y_true[i] == 1) & (y_score[i] == 1):
+            tp.append(i)
+        elif (y_true[i] == 1) & (y_score[i] == 0):
+            fn.append(i)            
+        elif (y_true[i] == 0) & (y_score[i] == 1):
+            fp.append(i)    
+    
+    req_stamp = pd.Series(y, index = ts)
+    req_stamp.plot(x=req_stamp.index, y=req_stamp.values, figsize=(12,6))
+    plt.scatter(x=tp, y=y[tp], c='red', label='True Positive')
+    plt.scatter(x=fp, y=y[fp], c='blue', label='False Positive')
+    plt.scatter(x=fn, y=y[fn], c='green', label='False Negative')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Value')
+    plt.title("Virtual Time Series")
