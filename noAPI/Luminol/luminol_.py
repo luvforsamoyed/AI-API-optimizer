c401a0d1cdf890484b96549c3e8782e5ea9cfebb
root <root@DESKTOP-HPORDNU.localdomain>
2019-05-30 00:08:34 +0900

    new datasets + utils_.inference_

--- noAPI/Luminol/luminol_.py
+++ noAPI/Luminol/luminol_.py
@@ -6,37 +6,54 @@ from datetime import datetime
 from luminol.anomaly_detector import AnomalyDetector
 from luminol.modules.time_series import TimeSeries
 from sklearn.metrics import f1_score
+from sklearn.metrics import average_precision_score
 
 class luminol():
     
-    def set_data(self, anomal_pt, value, time_series, threshold = None, algorithm=None):
+    def set_data(self, anomal_pt, value, time_series, percentile = None, algorithm=None):
         self.abn_pt = anomal_pt
         self.y = value
         self.ts = time_series
         self.algorithm = algorithm
-        self.threshold = threshold
+        self.percentile = percentile
         
     def f1_metrics(self):
         
         y = self.y
         ts = self.ts
         abn_pt = self.abn_pt
-        threshold = self.threshold
-        
         req_stamp = pd.Series(y, index = ts)
-        detector = AnomalyDetector(req_stamp.to_dict(), score_threshold=threshold)
-        anomalies = detector.get_anomalies()
-
+        detector = AnomalyDetector(req_stamp.to_dict())
+        scores = detector.get_all_scores()
+        
         y_true = np.zeros(y.size)
         for i in abn_pt:
             y_true[i] = 1    
         self.y_true = y_true
         
+        np_score = []
+        for i in scores.iteritems():
+            np_score.append(i[1])
+        req_ = pd.Series(data = np_score)        
+        
+        ap = average_precision_score(y_true, np_score)        
+        
+        range_ = np.log10(np.arange(0, 9, .1) + 1)
+        
+        f1 = []
+        for i in range_:
+            threshold = np.quantile(np_score, i)
+            anomalies = req_[req_.values > threshold].index.values
+
+            y_score = np.zeros(y.size)
+            for i in anomalies:
+                y_score[i] = 1
+            f1.append(f1_score(y_true, y_score))        
+        
+        threshold = np.quantile(np_score, range_[np.argmax(f1)])
+        anomalies = req_[req_.values > threshold].index.values
         y_score = np.zeros(y.size)
         for i in anomalies:
-            y_score[req_stamp.index.get_loc(i.exact_timestamp)] = 1
-        self.y_score = y_score
-        
-        f1 = f1_score(y_true, y_score)
+            y_score[i] = 1
 
-        return f1, y_true, y_score
+        return ap, y_true, y_score
--- utils_.py
+++ utils_.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import json, datetime
 from scipy.stats import norm
 
+from AWS import aws
+from noAPI.Luminol import luminol_
+from Azure import azure
+
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_curve
 
 def value_modeling(sample_size = 1000, seed = 0, cycle = 50, amp = 3, abn_num = 15, sig = 2):
 
@@ -16,7 +22,7 @@ def value_modeling(sample_size = 1000, seed = 0, cycle = 50, amp = 3, abn_num = 
     
     return abn_pt, y1
 
-def gen_time_idx(init_dt = datetime.datetime(2000, 1, 1), sample_size = 1000, dtype = 'datetime'):
+def gen_time_idx(init_dt = datetime.datetime(2000, 1, 1), sample_size = 1000, dtype = 'datetime', interval = 'hours'):
         
     if (dtype != 'datetime') & (dtype != 'timestamp'):
         print("Wrong dtype argument. Failed to generate time index.")
@@ -27,7 +33,7 @@ def gen_time_idx(init_dt = datetime.datetime(2000, 1, 1), sample_size = 1000, dt
     if dtype == 'timestamp':
         for i in range(sample_size):
             #add every 1day to make next point. iterate 1000 times
-            ith_dt = init_dt + i * datetime.timedelta(days=1)
+            ith_dt = init_dt + i * datetime.timedelta(hours=1)
             dt.append(datetime.datetime.timestamp(ith_dt))
 
         return dt
@@ -35,13 +41,17 @@ def gen_time_idx(init_dt = datetime.datetime(2000, 1, 1), sample_size = 1000, dt
     elif dtype == 'datetime':
         for i in range(sample_size):
             #add every 1day to make next point. iterate 1000 times
-            ith_dt = init_dt + i * datetime.timedelta(days=1)
+            ith_dt = init_dt + i * datetime.timedelta(hours=1)
             dt.append(ith_dt.isoformat())
         
         return dt
     
-def vis(y, dt, y_true, y_score):
-
+def vis(y, dt, y_true, y_score, type_):
+    
+    if (type_ != 'timestamp') and (type_ != 'datetime'):
+        print('Wrong time index type.')
+        return None
+    
     dt = np.array(dt)        
 
     tp = []
@@ -55,13 +65,80 @@ def vis(y, dt, y_true, y_score):
             fn.append(i)            
         elif (y_true[i] == 0) & (y_score[i] == 1):
             fp.append(i)    
-
-    req_stamp = pd.Series(y, index = dt)
-    req_stamp.plot(x=req_stamp.index, y=req_stamp.values, figsize=(12,6))
-    plt.scatter(x=dt[tp], y=y[tp], c='red', label='True Positive')
-    plt.scatter(x=dt[fp], y=y[fp], c='blue', label='False Positive')
-    plt.scatter(x=dt[fn], y=y[fn], c='green', label='False Negative')
+  
+    pd.Series(y, index = dt).plot(figsize=(15,6))
+    if type_ == 'timestamp':
+        plt.scatter(x=dt[tp], y=y[tp], c='red', label='True Positive')
+        plt.scatter(x=dt[fp], y=y[fp], c='blue', label='False Positive')
+        plt.scatter(x=dt[fn], y=y[fn], c='green', label='False Negative')
+    elif type_ == 'datetime':     
+        plt.scatter(x=tp, y=y[tp], c='red', label='True Positive')
+        plt.scatter(x=fp, y=y[fp], c='blue', label='False Positive')
+        plt.scatter(x=fn, y=y[fn], c='green', label='False Negative')        
     plt.legend()
     plt.xlabel('Time')
     plt.ylabel('Value')
-    plt.title("Time Series Detetion")
+    plt.title("Time Series Detetion for Best Epsilon")
+    
+def data_sel(branch='A1', num='1'):
+    branch_ = branch + 'Benchmark'
+    if branch == 'A1':
+        num_ = 'real_' + num + '.csv'
+    elif branch == 'A2':
+        num_ = 'synthetic_' + num + '.csv'
+    elif branch == 'A3':
+        num_ = 'A3Benchmark-TS' + num + '.csv'
+    elif branch == 'A4':
+        num_ = 'A4Benchmark-TS' + num + '.csv'
+    else:
+        print('Wrong Branch name.')
+        return None
+    name = branch_ + "/" + num_ 
+    return name
+
+def inference_(api, branch, num, graph=False):
+    ###data loading
+    name = data_sel(branch, num)
+    try:
+        real_1 = pd.read_csv('../anomaly_detection/dataset/' + name)
+    except:
+        return None
+    
+    sample_size_ = real_1.shape[0]
+        
+    #abn_pt
+    if (branch == 'A1') or (branch == 'A2'): 
+        abn_pt = real_1[real_1['is_anomaly'] == 1].index.values
+    elif (branch == 'A3') or (branch == 'A4'): 
+        abn_pt = real_1[real_1['anomaly'] == 1].index.values    
+    #y
+    y = real_1['value'].values
+    y_true = np.zeros(y.size)
+    for i in abn_pt:
+        y_true[i] = 1
+    
+    if api == 'luminol':
+        ts = gen_time_idx(dtype = 'timestamp', sample_size = sample_size_)
+        lm = luminol_.luminol()
+        lm.set_data(abn_pt, y, ts)
+
+        ap, lm_true, lm_score = lm.f1_metrics()
+        print("AP score for %s is: %f" %(api, ap))
+        if graph:
+            vis(y, ts, lm_true, lm_score, type_ = 'timestamp')
+
+    elif (api == 'aws') or (api == 'azure'):
+        dt = gen_time_idx(dtype = 'datetime', sample_size = sample_size_)
+        az = azure.azure()
+        az.set_data(abn_pt, y, dt, 
+                    url = "url", 
+                    endpoint = "endpoint", 
+                    key = "key",
+                    granularity='hourly')
+
+        ap, az_true, az_score = az.f1_metrics()  
+        print("AP score for %s is: %f" %(api, ap))
+        if graph:
+            vis(y, dt, az_true, az_score, type_ = 'datetime')
+        
+    return ap
